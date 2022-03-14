4f24c81890172174b41dbc24b7435e4186c04aea
olofaksberg <aksberg.olof@outlook.com>
2022-03-14 12:39:56 +0100

    nuf

--- app/front/src/utils/getDate&Time.js
+++ app/front/src/utils/getDate&Time.js
@@ -1,14 +1,14 @@
 export const getDateAndTime = () => {
   let date = new Date().toLocaleDateString().split("-");
-  let year = date[0].at(-2) + date[0].at(-1);
-  if (date[1].at(0) === "0" && date[2].at(0) === "0") {
+  let year = date[0].charAt(-2) + date[0].charAt(-1);
+  if (date[1].charAt(0) === "0" && date[2].charAt(0) === "0") {
     let day = date[2];
     let month = date[1];
     date = `${day[1]}/${month[1]}`;
-  } else if (date[1].at(0) === "0") {
+  } else if (date[1].charAt(0) === "0") {
     let month = date[1];
     date = `${date[2]}/${month[1]}`;
-  } else if (date[2].at(0) === "0") {
+  } else if (date[2].charAt(0) === "0") {
     let day = date[2];
     date = `${day[1]}/${date[1]}`;
   } else {
